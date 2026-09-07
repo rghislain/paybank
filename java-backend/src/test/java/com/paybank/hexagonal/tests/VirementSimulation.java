9b0fd7ab068e6997ccf70dca890466decc8c243d
ghislain <ghislainrochette44@gmail.com>
2026-09-07 18:20:49 +0200

    rg07092026 > ajout des outils de testings et d'évaluations, il ne reste plus qu'à nettoyer le code

---
+++ java-backend/src/test/java/com/paybank/hexagonal/tests/VirementSimulation.java
@@ -0,0 +1,26 @@
+package com.paybank.hexagonal.tests;
+
+import io.gatling.javaapi.core.Simulation;
+import io.gatling.javaapi.http.HttpProtocolBuilder;
+import io.gatling.javaapi.core.ScenarioBuilder;
+import static io.gatling.javaapi.core.CoreDsl.*;
+import static io.gatling.javaapi.http.HttpDsl.*;
+
+public class VirementSimulation extends Simulation {
+
+    HttpProtocolBuilder httpProtocol = http
+        .baseUrl("http://localhost:8080");
+
+    ScenarioBuilder scn = scenario("Scénario Charge Virement")
+        .exec(http("Requete Virement")
+            .post("/api/virements")
+            .header("Content-Type", "application/json")
+            .body(StringBody("{\"montant\": 100}"))
+        );
+
+    {
+        setUp(
+            scn.injectOpen(atOnceUsers(50))
+        ).protocols(httpProtocol);
+    }
+}
